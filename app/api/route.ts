5e5ab24792d7c1ceee41a29277113332ef9980e2
MKHLink <likhonhasankomol@gmail.com>
2025-01-13 13:13:37 -0500

    disabling ts any rule

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import dotenv from "dotenv";
 
 dotenv.config();
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 
 import {useState} from "react";
 import OutputCard from "./components/OutputCard";
